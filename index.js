db69183965d37d5fdea0f8f515d2b22338bc181a
Oleg Polak <pool42740@gmail.com>
2024-03-25 15:18:55 +0200

    added lesson10

---
+++ index.js
@@ -0,0 +1,17 @@
+import {name} from "./module";
+import './style.css';
+import isEmail from 'validator/lib/isEmail';
+import isURL from 'validator/lib/isURL';
+import toDate from 'validator/lib/toDate';
+
+console.log("i am: " + name);
+
+
+
+console.log(isEmail('foo@bar.com'));
+console.log(isURL('123456'));
+console.log(toDate('12.12.2012'));
+
+var moment = require('moment')
+
+console.log(moment().format("MMM Do YY"));
